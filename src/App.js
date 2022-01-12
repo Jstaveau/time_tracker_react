e2d6c146e10a30fdc771de7c331ce28fecf7799d
jonathan <jonathan.staveau@outlook.com>
2022-01-12 10:48:05 +0100

    creation contenant

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Global from './Components/Global/Global';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="App bg-bleu d-flex justify-content-center align-items-center">
+      <Global />
     </div>
   );
 }
---
+++ src/Components/Global/Global.js
@@ -0,0 +1,9 @@
+import './Global.css'
+
+export default function Global() {
+    return (
+        <section className="d-flex justify-content-center align-items-center col-8 m-auto bg-danger h-75">
+
+        </section>
+    )
+}
