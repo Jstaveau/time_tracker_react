7738808f0aeb858fe8d589525485017002bbc53f
jonathan <jonathan.staveau@outlook.com>
2022-01-12 11:57:59 +0100

    cards design ok

--- src/Components/Global/Global.js
+++ src/Components/Global/Global.js
@@ -1,10 +1,23 @@
 import './Global.css'
 import Utilisateur from '../Utilisateur/Utilisateur'
+import Cards from '../Cards/Cards'
 
 export default function Global() {
     return (
-        <section className="d-flex justify-content-center align-items-center col-8 m-auto bg-danger h-75">
+        <section className="height-section d-flex justify-content-center align-items-center col-8 m-auto bg-danger">
             <Utilisateur />
+            <div className='col-10 h-100 d-flex flex-column justify-content-between'>
+                <div className='height-cards d-flex justify-content-center'>
+                    <Cards />
+                    <Cards />
+                    <Cards />
+                </div>
+                <div className='height-cards d-flex justify-content-center'>
+                    <Cards />
+                    <Cards />
+                    <Cards />
+                </div>
+            </div>
         </section>
     )
 }
